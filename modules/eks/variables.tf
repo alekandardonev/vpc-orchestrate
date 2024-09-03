7ed7779412420e56a303cf85d9c5db8b683442cf
alekandardonev <alekandardonev@users.noreply.github.com>
2024-09-03 00:00:00 +0000

    Update footer content

---
+++ modules/eks/variables.tf
@@ -0,0 +1,22 @@
+variable "aws_region" {}
+
+variable "cluster_name" {}
+
+variable "cluster_key" {}
+
+variable "cluster_vpc" {}
+
+variable "k8s_version" {}
+
+variable "private_subnet_1a" {}
+
+variable "private_subnet_1c" {}
+
+variable "public_subnet_1a" {}
+
+variable "public_subnet_1c" {}
+
+variable "nodes_instances_sizes" {}
+
+variable "auto_scale_options" {} 
+
