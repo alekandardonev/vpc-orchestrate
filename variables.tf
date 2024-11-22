f17524d2ad53fe71ab04853d6b6280f62532833e
alekandardonev <alekandardonev@users.noreply.github.com>
2024-11-22 00:00:00 +0000

    Implement optimistic UI updates for faster perceived response

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "aws_region" {
+  description = "AWS Region for the VPC"
+  default     = "us-east-1"
+}
+
+variable "cluster_name" {
+  description = "Cluster name"
+  default     = "k8s-demo"
+}
+
+variable "aws_key_path" {
+  description = "SSH public key path"
+  default = "./resources/ssh/example.pub"
+}
+
+variable "k8s_version" {
+  description = "Kubernetes version"
+  default     = "1.15"
+}
+
+variable "nodes_instances_sizes" {
+  default = [
+      "t3.large"
+  ]
+}
+
+variable "auto_scale_options" {
+  default = {
+    min = 2
+    max = 10
+    desired = 2
+  }
+}
+
+
